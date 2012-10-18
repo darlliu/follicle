1ec2ed988bbe3b5e6a4ba22a4d7ac58e517ac6e8
darlliu <darlliu@lavabit.com>
2012-10-18 10:05:22 -0700

    day 2

--- src/follicle.cpp
+++ src/follicle.cpp
@@ -25,13 +25,78 @@ namespace fol
     {
         if ((sizeof (io)/sizeof (double))!=num_lig[pathway_num] \
                 || pathway_num>num_path)
-            throw ("access error: pathway mismatch")
+            throw ("access error: pathway size incorrect");
+        if (ligands[pathway_num]==NULL)
+            throw ("local storage array size incorrect");
         catch (char* info)
         {
             std::cout<<info<<std::endl;
             return;
         }
+        // the output format is [c1_ligand1, c1_ligand2 ... c2_ligand1...]
+
+        for (int i = 0; i<num_lig[pathway_num]; i++)
+            io[i]=ligands[pathway_num][i+num_lig[pathway_num]*cycle];
+    }
+
+    void follicle::cur_antagonists (unsigned int pathway_num, double * io)
+    {
+        if ((sizeof (io)/sizeof (double))!=num_antagonists[pathway_num] \
+                || pathway_num>num_path)
+            throw ("access error: pathway size incorrect");
+        try
+        {
+            if (antagonists[pathway_num]==NULL)
+            throw ("local storage array size incorrect");
+        }
+        catch (char* info)
+        {
+            std::cout<<info<<std::endl;
+            return;
+        }
+        // the output format is [c1_ligand1, c1_ligand2 ... c2_ligand1...]
+
+        for (int i = 0; i<num_antagonists[pathway_num]; i++)
+            io[i]=antagonists[pathway_num][i+num_antagonists[pathway_num]*cycle];
+    }
 
 
+    void follicle::cur_active_receptors (unsigned int pathway_num, double * io)
+    {
+        if ((sizeof (io)/sizeof (double))!=num_receptor[pathway_num] \
+                || pathway_num>num_path)
+            throw ("access error: pathway size incorrect");
+        if (num_receptor[pathway_num]==NULL)
+            throw ("local storage array size incorrect");
+        catch (char* info)
+        {
+            std::cout<<info<<std::endl;
+            return;
+        }
+        // the output format is [c1_ligand1, c1_ligand2 ... c2_ligand1...]
+
+        for (int i = 0; i<num_receptor[pathway_num]; i++)
+            io[i]=active_receptors[pathway_num][i+num_receptor[pathway_num]*cycle];
+    }
+
+    void follicle::set_next_ligands (unsigned int pathway_num, double * io)
+    {
+        if ((sizeof (io)/sizeof (double))!=num_lig[pathway_num] \
+                || pathway_num>num_path)
+            throw ("access error: pathway size incorrect");
+        if (ligands[pathway_num]==NULL)
+            throw ("local storage array size incorrect");
+        if (ligands[pathway_num][cycle]<0)
+            throw ("Wrong cycle number");
+
+        catch (char* info)
+        {
+            std::cout<<info<<std::endl;
+            return;
+        }
+        // the output format is [c1_ligand1, c1_ligand2 ... c2_ligand1...]
+
+        for (int i = 0; i<num_lig[pathway_num]; i++)
+            ligands[pathway_num][i+num_lig[pathway_num]*cycle]=io[i];
     }
 }
--- src/follicle.h
+++ src/follicle.h
@@ -39,6 +39,7 @@ namespace fol
             void cur_antagonists (unsigned int pathway_num, double * io);
             void cur_receptors (unsigned int pathway_num, double * io );
             void cur_active_receptors (unsigned int pathway_num, double * io);
+            
 
             double * all_ligands (unsigned int pathway_num) {return ligands[pathway_num];};
             double * all_antagonists (unsigned int pathway_num) {return antagonists[pathway_num];};
@@ -46,8 +47,17 @@ namespace fol
             double * all_active_receptors (unsigned int pathway_num){return active_receptors[pathway_num];};
 
             /* ====================  MUTATORS      ======================================= */
-            void evolve (); // all the local operations (determine state, birth and decay, noise)
-
+            void evolve (); 
+            // set the new state depending on our metric
+            
+            void set_next_ligands (unsigned int pathway_num, double *in);
+            void set_next_antagonists (unsigned int pathway_num, double *in);
+            void set_next_receptors (unsigned int pathway_num, double *in);
+            void set_next_active_receptors (unsigned int pathway_num, double *in);
+            
+            void generate_noise (double *mean_n, double *var_n, pathway_num);
+            // given the guassian mean and variance of noise,
+            // and the pathway number
             /* ====================  OPERATORS     ======================================= */
 
         protected:
@@ -58,18 +68,16 @@ namespace fol
             unsigned int num_path; //total number of pathways
             std::vector<unsigned int> num_lig, num_ang, num_receptor;
                                // number of ligands, antagonists, receptors
-            int* path_type;     // pathway types
+            double* path_type;     // pathway types, positive has greater than zero
             bool major;         // whether to output data row major or column major
             unsigned int index; // index of follicle, convertible to (x, y)
                                 // note: this shall become the element number (reordered) if meshed
 
             unsigned int rowl, coll; // row and column length of the grid
             states * state; // states if each cycle of this follicle
-            std::vector<double*> I, D, ligands, antagonists, receptors, active_receptors;  
+            std::vector<double*> ligands, ligands_noise, antagonists, antagonists_noise, receptors, active_receptors;  
             // synthesis, decay, ligand concentration (final), activator, receptor and receptor bound conc.
             // if more than one species per pathway then the species will be collapsed columnwise
-            double I_n[2], D_n[2], ligands_n[2],antagonists_n[2], receptors_n[2], active_receptors_n[2];  
-            // the guassian mean and var of noise in each term
             
 
         private:
