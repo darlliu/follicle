e98f3a3941185e8b1d4d32c465f3d96f77ba3fe0
darlliu <darlliu@lavabit.com>
2012-10-23 13:55:17 -0700

    reaction diffusion test code v0
    next do fol1d4 instead of fol1d3.

--- mtlab/fol1d3.m
+++ mtlab/fol1d3.m
@@ -5,50 +5,53 @@ N=20;
 x=linspace (0,1,N);
 % solve in [0,1]
 u=zeros(N,1);
+n=7;
 u(1)=0.0005;
+u(n)=0.0001;
 v=u;
 w=v;
 y=w;
 % discritize N pts along x
-M=20000;
+M=40000;
 M2=M;
 dt=0.002; %discritize time;
 U=zeros(N,M);
 V=U;
 W=V;
 Y=W;
+R1=U;
+R2=R1;
 %result matrix
 t=0:dt:dt*(M2-1);
 %time vector
-n=7;
+
 flag=0;
+counter=1000;
 toplot=ones(1,M);
 for i= 2: M,
     [gu,gv,gw,gy]=gen(u,v, w, y,n);
-    u=step(u,w,dt,3,gu,N);
-    w=step(w,u,dt,n,gw,N);
-    v=step(v,y,dt,n,gv,N);
-    y=step(y,v,dt,3,gy,N);
-    U(:,i)=u;
-    W(:,i)=w;
-    V(:,i)=v;
-    Y(:,i)=y;    
+    [U(:,i),R1(:,i)]=step(u,w,dt,5,gu,N,0.002,0.4,0.1);
+    W(:,i)=step(w,u,dt,n,gw,N,0.003,0.4,0.1);
+    [V(:,i),R2(:,i)]=step(v,y,dt,n,gv,N,0.0005,0.6,0.1);
+    Y(:,i)=step(y,v,dt,5,gy,N,0.002,0.6,0.1);
+    u=U(:,i);
+    w=W(:,i);
+    v=V(:,i);
+    y=Y(:,i);    
     toplot(i)=n;
-    [n,flag]=nextn(u,v,n,flag);
+    [n,flag,counter]=nextn(u,v,n,flag,counter);
 end
-size(U)
-size(V)
-size(t)
-size(x)
+
 figure
 plot(toplot)
 figure
-plot(t,sum(U),t,sum(V));
+plot(t,U(7,:),t,V(7,:));
+legend('BMP','WNT')
 
-legend('BMP','wnt')
+
 fig1=figure;
-plot(x,U(:,i),x,V(:,i),x,W(:,i),x,Y(:,i))
-legend('BMP','wnt','Noggin','Dkk')
+plot(x,U(:,i),x,V(:,i),x,W(:,i),x,Y(:,i),x,R1(:,i),x,R2(:,i))
+legend('BMP','wnt','Noggin','Dkk','Reaction 1','Reaction 2')
  windowsize=get(fig1,'Position');
  windowsize(1:2)=[0,0];
  Movie=moviein(100,fig1,windowsize);
@@ -60,8 +63,9 @@ j=2;
 for i=101:M/100:M,
     results(1,j)=u(toplot(j));
     results(2,j)=v(toplot(j));
-    plot(x,U(:,i),x,V(:,i),x,W(:,i),x,Y(:,i))
-legend('BMP','wnt','Noggin','Dkk')
+    plot(x,U(:,i),x,V(:,i),x,W(:,i),x,Y(:,i),x,R1(:,i),x,R2(:,i))
+    legend('BMP','wnt','Noggin','Dkk','Reaction 1','Reaction 2')
+    %plot(x,U(:,i))
     Movie(:,frame)=getframe(fig1,windowsize);
     frame=frame+1;
     j=j+1;
@@ -79,34 +83,37 @@ return
     
 
 function [gu,gv,gw,gy]=gen (u,v,w,y,n)
-gu=0.05*u(n)^2/(1+u(n)^2);
+gu=0.05*u(n)^2/(1+u(n)^2)+0.005;
 
-gw=0.04*w(n)^2/(1+w(n)^2);
+gw=0.02*w(n)^2/(1+w(n)^2)+0.004;
 
-gv=0.005*v(n)^2/(1+v(n)^2);
+gv=0.005*v(n)^2/(1+v(n)^2)+0.002;
 
-gy=0.01*y(n)^2/(1+y(n)^2);
+gy=0.01*y(n)^2/(1+y(n)^2)+0.001;
 
 return
 
 
-function u=step(u,w,dt,n,gu,N)
+function [u,R]=step(u,w,dt,n,gu,N,ds,rs,des)
 %backward euler
 dx=1/N;
 D=-2*eye(N,N)+diag(diag(eye(N-1,N-1)),1)+diag(diag(eye(N-1,N-1)),-1);
 D(1,1)=-1;
-D(1,2)=0;
+D(1,2)=1;
 D(N-1,N-1)=-1;
 D(N,N)=0;
+%D(N,N-1)=0;
+%D(N,N-2)=0;
 D(N,N-1)=-1;
 D(N,N-2)=1;
-D=D*0.01;
+D=D*ds;
 %D with mixed neumann condition
 F=zeros(N,1);
 F(n)=gu;
 %F(0) is the source of diffusion.
 I=eye(N,N);
-R=-0.005*(u.*w./(1+u.*w)).*u-0.005*u;
+%R=zeros(N,1);
+R=-rs*sqrt((w).*u)-des*u;
 u =(I-(dt/dx^2)*D)\(u+dt*F+R*dt);
 %unext=(D*u+F).*(dt/dx^2);
 return
--- mtlab/nextn.m
+++ mtlab/nextn.m
@@ -1,15 +1,21 @@
-function [nout,flagout]=nextn(u,v,n,flag)
-if (u(n)<0.8*v(n)),
-    if n<15 && flag==0,
-        n=n+1;
-    end
+function [nout,flagout,cnt]=nextn(u,v,n,flag,counter)
+cnt=counter-1;
+if cnt>1,
+    nout=n;
+    flagout=flag;
+    return
+else
+    cnt=1000;
+end
+if flag==0 && n==7 && (u(7)<1.3E-3 && v(7)>5E-3),
+    n=n+1;
     flagout=flag;
     nout=n;
     return
 end 
 if n>7 && n<15 && flag==0,       
     n=n+1;
-elseif n==15 &&flag==0,
+elseif n==15 && flag==0,
     n=n-1;
     flag=1;
 elseif flag==1 && n>7,
