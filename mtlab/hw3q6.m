838eeafd6090e51c7acfbef192d6dfe74964f32d
darlliu <darlliu@lavabit.com>
2012-10-26 00:42:03 -0700

    hws

--- mtlab/hw3q6.m
+++ mtlab/hw3q6.m
@@ -1,15 +1,15 @@
 syms x y
-X= x*(x-y);
-Y= y*(2*x-y);
+X= x*(2-x-y);
+Y= x-y;
 figure
 F=[X Y];
 vectline (F,[x y],[-10 10 -10 10]);
 u=-10:0.01:10;
 
 hold on
-ezplot('x*(x-y)',[-10,10,-10,10])
-ezplot('y*(2*x-y)',[-10,10,-10,10]);
+ezplot('x*(2-x-y)',[-10,10,-10,10])
+ezplot('x-y',[-10,10,-10,10]);
 axis([-10 10 -10 10])
 
 legend('vector field','xnullcline','ynullcline')
-title('phase portrait of the system 6.1.3')
+title('phase portrait of the system 6.1.5')
