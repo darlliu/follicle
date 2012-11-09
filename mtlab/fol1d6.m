43aa85b241d63ba554e676f41022e81f6779f171
darlliu <darlliu@lavabit.com>
2012-11-08 16:16:36 -0800

    need to completely revamp the numerical method, ouch.

--- mtlab/fol1d6.m
+++ mtlab/fol1d6.m
@@ -1,5 +1,5 @@
-function fol1d5
-% two sources model v2
+function fol1d6
+%another attempt at the older model.
 
 clc
 close all
@@ -35,6 +35,9 @@ Src=[10 11];
 Rec=[40 41];
 %receptors source
 
+Prc=[];
+%precortex
+
 Ends=[50 51];
 %the initial ends;
 
@@ -55,16 +58,9 @@ W=V;
 Y=W;
 %total concentration-time matrix of ligands
 
-%D=MakeLaplacian1D(N);
+D=MakeLaplacian1D(N);
 % diffusion matrix
-D=-2*eye(N,N)+diag(diag(eye(N-1,N-1)),1)+diag(diag(eye(N-1,N-1)),-1);
 
-D(1,2)=2;
-
-
-%D(N,N-1)=0;
-%D(N,N-2)=0;
-D(N,N-1)=2;
 
 t=0:dt:dt*(M-1);
 %time vector
@@ -74,18 +70,19 @@ t=0:dt:dt*(M-1);
 Rtot1=1E-3;
 Rtot2=Rtot1;
 %max receptor amount
-kon1=5;
-koff1=1;
-kon2=5;
-koff2=1;
+kon1=5E-2;
+koff1=1E-2;
+kon2=5E-2;
+koff2=1E-2;
 kdeg1=1E-3;
 kdeg2=1E-3;
-kg1=4E-7;
-kg2=3E-7;
-kgen1=2E-5;
+kg1=1E-7;
+kg2=4E-7;
+kgen1=2.1E-5;
 kgen2=2E-5;
 kr1=140;
 kr2=140;
+flag=0;
 %rates
 % a1=2;
 % a2=2;
@@ -100,25 +97,39 @@ for i= 2: M,
     gu=rxn(u,r1,kon1,koff1,Rtot1,Rec,dt,N);
     gv=rxn(v,r2,kon2,koff2,Rtot2,Rec,dt,N);
     if counter==1,
-        Ends=Ends+grow(r1,r2,kg1,kg2);
-
+        [n,flag]=grow(r1,r2,kg1,kg2,flag);
+        Ends=Ends+n;
+        if Ends(2)>75,
+            Prc=Ends-2;
+        else
+            Prc=[];
+        end
         if Ends(2)>=100,
             Ends=[99,100];
         end
         if Ends(1)<=50,
             Ends=[50,51];
+            flag=0;
         end
         counter=100;
     end
     counter=counter-1;
     H(i)=Ends(1);
-    [gu2,gv2]=gen(r1,r2,kgen1,kgen2,Ends,Src,N,dt);
-    [gw2,gy2]=gen(r1,r2,kgen1,kgen2,Src,Ends,N,dt);
+    gu2=gen(kgen1*0.2,Src,N,dt);
+    gv2=gen(kgen2*0.3,Ends,N,dt);
+    gv2=gv2+gen(kgen2*0.3,Src,N,dt);
+    gw2=gen(kgen1*0.9,Ends,N,dt);
+    gy2=gen(kgen2*0.2,Src,N,dt);
+    if isempty(Prc),
+    else
+        gu2=gu2+gen(kgen1*0.5,Prc,N,dt);
+        gy2=gy2+gen(kgen2*0.9,Prc,N,dt);
+    end
     [gu3,gw3]=rxn2(u,w,kr1,dt);
     [gv3,gy3]=rxn2(v,y,kr2,dt);
     
-    U(:,i)=step(d,D,u,-kdeg1*u+gu+gu2+gu3,dx,dt,N);
-    V(:,i)=step(d,D,v,-kdeg1*v+gv+gv2+gv3,dx,dt,N);
+    U(:,i)=step(d,D,u,-kdeg1*u*dt+gu+gu2+gu3,dx,dt,N);
+    V(:,i)=step(d,D,v,-kdeg1*v*dt+gv+gv2+gv3,dx,dt,N);
     W(:,i)=step(d,D,w,-kdeg1*w*dt+gw2+gw3,dx,dt,N);
     Y(:,i)=step(d,D,y,-kdeg1*y*dt+gy2+gy3,dx,dt,N);
     u=U(:,i);
@@ -173,16 +184,22 @@ end
 
 % size(Stem)
  movie(fig1, Movie, 100,8,windowsize);
+ 
 return
 
-function n=grow(r1,r2,kg1,kg2)
+function [n,flag]=grow(r1,r2,kg1,kg2,flag)
 %this part is tricky. must return n integer and n<N
+if flag==1,
+    n=-1;
+    return;
+end
 if sum(r1)<kg1 && sum(r2)>kg2,
     n=1;
-%elseif sum(r1)>kg1 && sum(r2)<kg2,
-%    n=-1;
-else
+elseif sum(r2)<kg2/2,
     n=-1;
+    flag=1;
+else
+    n=0;
 end
 
 return
@@ -193,22 +210,21 @@ gr(Rec)=dt*(-kon*u(Rec).*(rtot-r)+koff*(r));
 
 return
 
-function [gu2,gv2]=gen(r1,r2,kgen1,kgen2,Ends,Src,N,dt)
+function gu2=gen(kgen1,Src,N,dt)
 gu2=zeros(N,1);
-gv2=gu2;
 gu2(Src)=dt*kgen1;
-gv2(Ends)=dt*kgen2;
+
 %gu2(Src)=dt*(sum(r1)/(kgen1+sum(r1)));
 %gv2(Ends)=dt*(sum(r2)/(kgen2+sum(r2)));
 return
 
 
 function u=step(d,D,u,F,dx,dt,N)
-I=eye(N,N);
-%I=eye(N-1,N-1);
+%I=eye(N,N);
+I=eye(N-1,N-1);
 %R=zeros(N,1);
-%u(1:N-1) =(I-d*(dt/dx^2)*D)\(u(1:N-1)+F(1:N-1));
-u =(I-d*(dt/dx^2)*D)\(u+F);
+u(1:N-1) =(I-d*(dt/dx^2)*D)\(u(1:N-1)+F(1:N-1));
+%u =(I-d*(dt/dx^2)*D)\(u+F);
 return
 
 function [gv3,gy3]=rxn2(v,y,kr2,dt)
